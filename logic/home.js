43f14d073ef9537ae90219764813f9840069c44f
AAyushGuptAA <professorayushgupta@gmail.com>
2021-06-09 17:40:02 +0530

    navbar and minor issues fixed

--- logic/home.js
+++ logic/home.js
@@ -1,15 +1,20 @@
+const header = document.querySelector('header');
 const toggler = document.querySelector('.ham');
-const nav = document.querySelector('nav');
+const nav = document.querySelector('.navbar');
 const anchors = document.querySelectorAll('a');
 toggler.addEventListener('click', () => {
+    header.classList.toggle('hide');
     nav.classList.toggle('toggle');
     toggler.classList.toggle('reverse');
 })
 
 anchors.forEach(a => {
     a.addEventListener('click', () => {
-        nav.classList.toggle('toggle');
-        toggler.classList.toggle('reverse');
+        if (!a.parentElement.classList.contains('drop')) {
+            header.classList.toggle('hide');
+            nav.classList.toggle('toggle');
+            toggler.classList.toggle('reverse');
+        }
     })
 })
 
