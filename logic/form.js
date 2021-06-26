4b74b45a5d2048b869b47d644bee9f518a21a3a9
AAyushGuptAA <professorayushgupta@gmail.com>
2021-06-26 23:58:45 +0530

    favicon added, competitions page ready, gymkhana link added

---
+++ logic/form.js
@@ -0,0 +1,6 @@
+// const form = document.querySelector('form');
+// const nameField = document.getElementById('name');
+// const emailField = document.getElementById('email');
+// const queryField = document.getElementById('query');
+// form.addEventListener('submit', (e) => {
+// })
